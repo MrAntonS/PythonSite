c7866e1ffdadd8a3e917a500bbb918e90c1600dd
Anton <mrantonsaenko@gmail.com>
2022-07-24 20:02:40 -0400

    Installed couple libs

--- MyOwnSite/MyOwnSite/urls.py
+++ MyOwnSite/MyOwnSite/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include, re_path
+from MyCard.views import index
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('news/', include("news.urls")),
+    path('index/', index),
 ]
 
 
